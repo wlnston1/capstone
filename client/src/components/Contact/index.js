f94d703afe6c190171c6b5bee747e98b63975993
Winston Tang <winstontang@Winstons-MBP-16.attlocal.net>
2023-03-13 16:03:59 -0700

    update css and handle

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -6,28 +6,42 @@ import Flag from "../Flag";
 
 function Contact() {
 
+  const [selectedId, setSelectedId] = useState(null);
+  const [contactStat, setContactStat] = useState(null);
+  const [flag, setFlag] = useState(null);
+  function handleSelectId(userId) {
+    setSelectedId(userId)
+  }
+
+  function handleContactStat(stat) {
+    setContactStat(stat)
+  }
+
+  function handleFlag(flag) {
+    flag = flag.toLowerCase();
+    setFlag(flag);
+  }
 
 
   return (
     <div>
-      <div className="top-part">
-        <div className="contact-inner">
-          <h4>Total Employees</h4>
-          <p> 456 </p>
+      <div className=".text-center row info-sec">
+        <div className="contact-inner col col-4">
+          <StatBox stat={contactStat}> </StatBox>
         </div>
 
-        <div className="contact-inner">
-          <h4>Age Ranges</h4>
-          <h6> From 24 to 64</h6>
+        <div className="contact-inner col col-4">
+          <h4>Country</h4>
+          <Flag flag={flag}/>
         </div>
-        <div className="contact-inner">
-          <InfoBox></InfoBox>
+        <div className="contact-inner col col-4">
+          <InfoBox selectId={selectedId}></InfoBox>
         </div>
       </div>
 
       <div className="bot-part">
         <div className="bot-inner">
-          <Employee></Employee>
+          <Employee onFlag={handleFlag} onSelectId={handleSelectId} onContactStat={handleContactStat}></Employee>
         </div>
       </div>
     </div>
