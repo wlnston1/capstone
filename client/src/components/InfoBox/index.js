30150787cc41c4e24399a362d16a939a8b80e7d1
Winston Tang <winstontang@Winstons-MBP-16.attlocal.net>
2023-03-13 19:42:18 -0700

    feat: infobox >index.js

--- client/src/components/InfoBox/index.js
+++ client/src/components/InfoBox/index.js
@@ -1,16 +1,49 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-function InfoBox() {
+function InfoBox({selectId}) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (!selectId) {
+      // No ID is selected, reset the data state
+      setData(null);
+      console.log("here");
+      return;
+    }
+
+    async function fetchData() {
+      try {
+        const response = await fetch(`http://localhost:8888/api/contact/${selectId}`);
+        const data = await response.json();
+        setData(data);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, [selectId]);
+
   return (
     <div>
-      <div className="top-part">
-        
-        {/* <p>{user.phone}</p>
-                <p>{user.age}</p>
-                <p>{user.city}</p>
-                <p>{user.country}</p> */}
-        <p>?</p>
-      </div>
+      {selectId ? (
+        <div>
+          <h4>User</h4>
+          {/* <p>{selectId}</p> */}
+          <p>Age : {data?.age}</p>
+          <p>Phone number: {data?.phone}</p>
+          <p>Experience : {data?.experience}</p>
+          <p>Country : {data?.country}</p>
+        </div>
+      ) : (
+        <div>
+          
+        <h4>User</h4>
+        <p>No user selected</p>
+        </div>
+      )}
     </div>
   );
 }
