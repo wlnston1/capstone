695fd64cda4140d0163b3c73beaea767f70dd35a
Winston Tang <winstontang@Winstons-MBP-16.attlocal.net>
2023-03-14 02:29:57 -0700

    ft: updated wording on dashboard

--- client/src/components/Statbox/index.js
+++ client/src/components/Statbox/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function StatBox({ stat }) {
   return (
     <div>
-      <h4>Overall View</h4>
+      <h4>Overview</h4>
       <div className="mt-3">
         <p> Total Employees: {stat?.total} </p>
         <p> Average Experience: {stat?.avgExp} </p>
