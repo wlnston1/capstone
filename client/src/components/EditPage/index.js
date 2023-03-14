a38d49f6faddf107b5ee52c26cbeb8107db5d929
Winston Tang <winstontang@Winstons-MBP-16.attlocal.net>
2023-03-14 02:14:19 -0700

    ft: installed tailwindcss

--- client/src/components/EditPage/index.js
+++ client/src/components/EditPage/index.js
@@ -124,7 +124,7 @@ axios.put(`http://localhost:8888/api/contact/${userId}`, (user))
             />
           </div>
 
-          <button type="submit">Update User</button>
+          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Update User</button>
         </form>
       </div>
     );
