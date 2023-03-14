71a2b3bafbed7497f7ae2ef979b8266a91479212
Winston Tang <winstontang@Winstons-MBP-16.attlocal.net>
2023-03-13 17:42:31 -0700

    feat: clients>components>employee>index

--- client/src/components/Employee/index.js
+++ client/src/components/Employee/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-function Employee({ user }) {
+function Employee({ onSelectId, onContactStat, onFlag }) {
   const [users, setUser] = useState([]);
 
   useEffect(() => {
@@ -12,8 +12,20 @@ function Employee({ user }) {
 
   const getUsers = async () => {
     const response = await axios.get("http://localhost:8888/api/contact");
-    setUser(response.data);
-    console.log(response.data);
+    const data = response.data;
+    setUser(data);
+
+    // call props handler function
+    var statData = {};
+    statData.total = data.length;
+
+    var totalExp = 0;
+    data.map((item) => {
+      totalExp += item.experience;
+    });
+
+    statData.avgExp = Math.round((totalExp / data.length) * 100) / 100;
+    onContactStat(statData);
   };
 
   const deleteUser = async (id) => {
@@ -24,10 +36,22 @@ function Employee({ user }) {
   return (
     <div>
       <div className="employee">
+        <div className="employee-inner row my-2">
+          <div  className="col col-4">
+            <h4 style={{marginLeft:"-40px"}}>Name</h4>
+          </div>
+          <div className="col col-3">
+            <h4>Email</h4>
+          </div>
+          <div className="col col-5">
+            <h4>Actions</h4>
+          </div>
+        </div>
+
         <div className="">
           {users.map((user) => {
             return (
-              <div id={user.id} className="employee-inner row my-2">
+              <div key={user._id} className="employee-inner row my-2">
                 <div
                   className="col col-4"
                   style={{
@@ -40,7 +64,7 @@ function Employee({ user }) {
                 <div className="col col-3">
                   <p>{user.email}</p>
                 </div>
-             
+
                 <div className="employee-bt col col-5 .text-right">
                   <button
                     className="btn btn-danger mx-4 my-2"
@@ -48,14 +72,22 @@ function Employee({ user }) {
                   >
                     Delete
                   </button>
-                  <Link to={`/api/contact/edit/${user._id}`}>
+                  
+                  <Link to={`/edit/${user._id}`}>
                     <button className="btn btn-secondary mx-4 my-2">
                       Edit
                     </button>
                   </Link>
-                  <Link to={`/api/contact/${user._id}`}>
-                    <button className="btn btn-info mx-4 my-2">Select</button>
-                  </Link>
+
+                  <button
+                    className="btn btn-info mx-4 my-2"
+                    onClick={() => {
+                      onSelectId(user._id);
+                      onFlag(user.country);
+                    }}
+                  >
+                    select
+                  </button>
                 </div>
               </div>
             );
