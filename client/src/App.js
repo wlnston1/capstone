79e4c324a022b0f81a67e34999e20a9eec9ae500
Winston Tang <winstontang@Winstons-MBP-16.attlocal.net>
2023-03-13 20:45:04 -0700

    feat: app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,39 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
-import Dashboard from "./components/Dashboard";
+import Employment from "./components/Employment";
 import Services from "./components/Service";
 import Contact from "./components/Contact";
+import EditPage from "./components/EditPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container px-3">
       {/* sidebar */}
 
-      {/*  dashboard */}
-      <Router>
-        <div className="sidebar">
-          <Sidebar></Sidebar>
-        </div>
-        <Routes>
-          <Route path="/" exact element={<Dashboard />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/service" element={<Services />} />
-          <Route path="/service" element={<Services />} />
-          <Route path="*" element={<p>Path not resolved</p>} />
-        </Routes>
-      </Router>
+      {/*  Employment */}
+      <div className="row mb-5">
+        <h1>Employee Management</h1>
+      </div>
+      <div className="row">
+        <Router>
+          <div className="sidebar col col-3">
+            <Sidebar></Sidebar>
+          </div>
+          <div className="col col-9">
+            <Routes>
+              <Route path="/" element={<Employment />} />
+              <Route exact path="/contact" element={<Contact />} />
+              <Route path="/edit/:userId" element={<EditPage />} />
+              <Route path="/service" element={<Services />} />
+              <Route path="*" element={<p>Path not resolved</p>} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
 
-      {/* <div className="dashboard">
-        <Dashboard></Dashboard>
+      {/* <div className="Employment">
+        <Employment></Employment>
       </div> */}
     </div>
   );
